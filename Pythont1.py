9a4ef98ed9c9249fd2ae4871f91bd6dafdfdf424
Pavithra519 <81804936+Pavithra519@users.noreply.github.com>
2021-04-04 11:58:01 +0530

    Add files via upload

---
+++ Pythont1.py
@@ -0,0 +1,9 @@
+def printinfo(arg1,*vartuple):
+    "This prints a variable passed arguments"
+    print("Output is :")
+    print(arg1)
+    for var in vartuple:
+        print (var)
+    return;
+print(10)
+print(70,60,50)
