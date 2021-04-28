093355c1b50b9c7b642c6aa242d8c7082485e844
Pavithra519 <81804936+Pavithra519@users.noreply.github.com>
2021-04-28 11:03:45 +0530

    Add files via upload

---
+++ fristselenium.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("http://www.facebook.com")
+driver.find_element_by_xpath("//*[@id='email']").send_keys('pavithra')
+print("Application title is",driver.title)
+print("Application url is",driver.current_url)
+driver.quit()
